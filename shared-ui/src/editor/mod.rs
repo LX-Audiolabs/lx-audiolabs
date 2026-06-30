4db6225a325c7d1f2938f0dfa1fea7a5bd7672fb
lxndrbe <ardvinnamoon@gmail.com>
2026-06-30 12:56:18 +0200

    refactor(shared-ui): remove dead nice-plug editor module, project is pure truce-iced now

--- shared-ui/src/editor/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod notifier;
-pub mod factory;
-pub mod common;
-
-pub use notifier::gui_timer;
-pub use factory::{create_lx_editor, LxEditorApp};
-pub use common::CommonEditorState;
--- shared-ui/src/editor/notifier.rs
+++
@@ -1,1 +0,0 @@
-pub use iced_futures::backend::default::time as gui_timer;
